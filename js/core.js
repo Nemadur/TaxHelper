938df6678b8b943be312f5cb62bdc1feb7157d16
Jan Piekos <jpiekos@topsolution.pl>
2020-04-14 10:58:27 +0200

    resolve fix

--- js/core.js
+++ js/core.js
@@ -37,11 +37,7 @@ function core_init() {
                 xhttp.onreadystatechange = function() {
             
                     if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("result").innerHTML =
-                        this.responseText;
-            
                         let data = JSON.parse(this.responseText);
-            
                         resolve(data);
                     }
                 };
@@ -51,21 +47,6 @@ function core_init() {
             }
         });
     }
-
-    function loadXMLDoc(url) {
-        var xhttp = new XMLHttpRequest();
-        
-        xhttp.onreadystatechange = function() {
-    
-            if (this.readyState == 4 && this.status == 200) {
-                return JSON.parse(this.responseText);
-            }
-        };
-    
-        xhttp.open("GET", url, true);
-        xhttp.send();
-    }
-    
 
     // CSV FILE
     
